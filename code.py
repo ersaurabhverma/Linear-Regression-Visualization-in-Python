6da21c924fc7f5ab7b9ce1459b350f7ea5e3a33d
Saurabh Verma <verma.saurabh8010@gmail.com>
2020-04-17 22:41:08 +0530

    update code.py

--- code.py
+++ code.py
@@ -3,20 +3,32 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 x_ = []
 y_ = []
-
+font = cv2.FONT_HERSHEY_SIMPLEX 
+org1 = (20, 50) 
+org2 = (20, 70) 
+org3 = (20, 90)
+fontScale = 0.5
+color = (255, 255, 255) 
+thickness = 1
 
 def draw_circle(event, x, y, flags, param):
-
     if event == cv2.EVENT_LBUTTONDBLCLK:
         x_.append(x)
         y_.append(y)
         lr = LinearRegression()
         lr.fit(np.array(x_).reshape(-1, 1), np.array(y_).reshape(-1, 1))
-        m = lr.coef_
-        c = lr.intercept_
-
+        m = lr.coef_[0][0]
+        c = lr.intercept_[0]
+        print(m,c)
         img[img == 255] = 0
-        cv2.line(img, (0, c), (800, m*800+c), (0, 0, 255), 2)
+        cv2.putText(img, f'Slope = {m:.4f}', org1, font,  
+                   fontScale, color, thickness) 
+        
+        cv2.putText(img, f'Intercept = {c:.4f}', org2, font,  
+                   fontScale, color, thickness) 
+        cv2.putText(img, f'Y = {m:.4f} * X + {c:.4f}', org3, font,  
+                   fontScale, color, thickness) 
+        cv2.line(img, (0, int(c)), (800, int(m*800+c)), (0, 0, 255), 2)
         for ix, iy in zip(x_, y_):
             cv2.circle(img, (ix, iy), 5, (0, 200, 0), -1)
 
